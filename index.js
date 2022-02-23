4bd3f53c522abb9519cce81afef70f44da4c284b
Allison <amh03160@gmail.com>
2022-02-23 09:14:49 -0600

    commit tree 3ebddf08f5f7e88f98e1501f6a29b7c0a1636bd9

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickUp){
+const distance = pickUp - 42;
+return Math.abs(distance);
+}
+
+function distanceFromHqInFeet(pickUp){
+const blockDistance = distanceFromHqInBlocks(pickUp);
+const ftDistance = blockDistance*264;
+    return ftDistance;
+}
+
+function distanceTravelledInFeet(start, destination){
+const traveled = (destination-start)*264;
+return Math.abs(traveled);
+}
+
+function calculatesFarePrice(start,destination) {
+ const distanceTravelled = distanceTravelledInFeet(start,destination);
+ if(distanceTravelled<400){
+     return 0
+ } else if (distanceTravelled > 400 && distanceTravelled < 2000){
+    const chargedDist = distanceTravelled - 400; 
+    return chargedDist*.02;
+ } else if (distanceTravelled < 2500 && distanceTravelled > 2000){
+     return 25;
+ } else {
+     return "cannot travel that far";
+ }
+}
